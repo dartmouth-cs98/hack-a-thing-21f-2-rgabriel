aad0e23ec19906422b0c89205241c312320f512c
Roberto Gabriel Brito <roberto.a.gabriel.brito.22@dartmouth.edu>
2021-09-27 18:07:35 -0400

    implement println! macro using writer

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 13).unwrap();
+    println!("Hello, world{}", "!");
+    panic!("panicked at 'Some panic message', src/main.rs:28:5");
 
     loop {}
 }
 
 /// Loops indefinitely on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop{}
 }
