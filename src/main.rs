8c700ed9127b886b18f457d24d8c0d7800450474
Roberto Gabriel Brito <roberto.a.gabriel.brito.22@dartmouth.edu>
2021-09-28 18:46:50 -0400

    set qemu to exit after testing is done

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
@@ -9,7 +10,9 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, world{}", "!");
-    panic!("panicked at 'Some panic message', src/main.rs:28:5");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -27,5 +30,30 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
     for test in tests {
         test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial Assertion: ");
+    assert_eq!(1, 1);
+    println!("[OK]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
